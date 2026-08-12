30a67fc7b47e29297cf5e453762ef8495d8d83e4
Benjamin Naidoo <naidoobenjamin1313@gmail.com>
2026-08-12 19:57:12 +0200

    Uploaded Lists project files

---
+++ ListsHomeworkApp/ListsHomework/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+// Add students to a class roster until there are no more students to add
+// Display count of students in class
+
+string? studentName = null;
+List<string> students = new List<string>();
+
+do
+{
+    Console.Write("Enter Student (Type \"done\" to stop): ");
+    studentName = Console.ReadLine();
+
+    if (!String.IsNullOrEmpty(studentName) && studentName.ToLower() != "done")
+    {
+        students.Add(studentName);
+        Console.WriteLine("Student Added...");
+    }
+}
+while (studentName.ToLower() != "done");
+
+Console.WriteLine($"Total number of students entered is: {students.Count}");
+
